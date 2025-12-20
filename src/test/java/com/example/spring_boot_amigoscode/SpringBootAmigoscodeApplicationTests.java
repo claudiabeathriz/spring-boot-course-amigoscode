c3437edd4279dbac579045d0ec4c22de070a3eb7
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2025-12-20 14:30:00 -0300

    Initial Spring Boot project setup

---
+++ src/test/java/com/example/spring_boot_amigoscode/SpringBootAmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_amigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAmigoscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
